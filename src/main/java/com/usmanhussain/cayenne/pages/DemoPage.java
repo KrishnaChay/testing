226da962ad1d0dc76f9d5a79c599466b96b897bd
KrishnaChaitanya <krishna.chaitanya@directlinegroup.co.uk>
2018-05-21 22:33:40 +0100

    test

--- src/main/java/com/usmanhussain/cayenne/pages/DemoPage.java
+++ src/main/java/com/usmanhussain/cayenne/pages/DemoPage.java
@@ -1,6 +1,14 @@
 package com.usmanhussain.cayenne.pages;
 
 import com.usmanhussain.habanero.framework.AbstractPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class DemoPage extends AbstractPage {
+    
+
+    public WebElement testElement() {
+        return waitAndFindElement(By.id("test"));
+
+    }
 }
